2615ba2e407e907d4e034b1f3eb472992ce6986f
Tracy <8510537+tracychen@users.noreply.github.com>
2022-10-09 03:18:55 -0500

    feat(frontend): sign tx when submitting form response

--- frontend/src/views/Answer/Answer.tsx
+++ frontend/src/views/Answer/Answer.tsx
@@ -5,11 +5,13 @@ import { useEffect, useState } from "react";
 import AnswerForm from "./AnswerForm";
 import { useWeb3Auth } from "../../hooks/useWeb3Auth";
 import { checkOwner } from "../../api/nfts";
+import { ethers } from "ethers";
+import { Deformed__factory } from "@deformed/contracts";
+
 interface Token {
   contractAddress: string;
   tokenId: string;
 }
-
 
 export type CredentialNFT = {
   contractAddress: string;
@@ -17,19 +19,22 @@ export type CredentialNFT = {
   imageUrl: string;
   name: string;
   description: string;
-}
+};
 
 const Answer = ({ formId }: any) => {
   const { address } = useWeb3Auth();
   const [formFields, setFormFields] = useState<any[]>([]);
   const [hasAccess, setHasAccess] = useState<boolean>(false);
+
+  const { web3authProvider } = useWeb3Auth();
+
   const submitAnswer = async (rawFormAnswerDict: any) => {
     /**
      * The form answers will come in the following format:
      * {
      *  question-0: '1asdf'
      * }
-     * 
+     *
      * We need the API POST body to be in this format instead:
      * [
      *  {
@@ -39,28 +44,35 @@ const Answer = ({ formId }: any) => {
      * ]
      */
     try {
-      const createAnswerFormAnswers: any[]= [];
+      const createAnswerFormAnswers: any[] = [];
       const keys = Object.keys(rawFormAnswerDict);
       keys.forEach((key) => {
         const answer = rawFormAnswerDict[key];
-        createAnswerFormAnswers.push(
-          {
-            "referenceId": key,
-            "value": answer,
-          }
-        )
-      })
-      await createAnswer({
+        createAnswerFormAnswers.push({
+          referenceId: key,
+          value: answer,
+        });
+      });
+      const response = await createAnswer({
         formId: formId,
         formAnswers: createAnswerFormAnswers,
       });
+      const provider = new ethers.providers.Web3Provider(web3authProvider);
+      const deformed = Deformed__factory.connect(
+        "0xfFAfd3b46D3034bB3f12868c92DCA375E7263C38",
+        provider.getSigner()
+      );
+      await deformed.submitFormResponse(formId, response.hash);
     } catch (error: any) {
       console.log(error);
     }
   };
 
   // returns true if the loggedInAddress owns *all* of the requiredTokens
-  const checkAccessTokens = async (requiredTokens: Array<Token>, loggedInAddress: string) => {
+  const checkAccessTokens = async (
+    requiredTokens: Array<Token>,
+    loggedInAddress: string
+  ) => {
     console.log("loggedInAddress: ", loggedInAddress);
     console.log("checking access tokens");
     for (const token of requiredTokens) {
@@ -78,8 +90,7 @@ const Answer = ({ formId }: any) => {
     console.log("all tokens owned");
     // return true if all tokens are owned by logged in user
     return true;
-  }
-
+  };
 
   // obtain the form config from the backend API to populate the form itself
   useEffect(() => {
@@ -87,12 +98,13 @@ const Answer = ({ formId }: any) => {
       if (formId) {
         const form = await getForm(formId);
         setFormFields(form.form.fields);
-        setHasAccess(await checkAccessTokens(form.accessControlTokens, address));
+        setHasAccess(
+          await checkAccessTokens(form.accessControlTokens, address)
+        );
       }
     };
     obtainForm();
   }, [formId]);
-
 
   return (
     <>
@@ -101,7 +113,7 @@ const Answer = ({ formId }: any) => {
           <Card className="rounded py-10 px-12 bg-white flex-col shadow-md">
             <div>You do not have access to all of the required tokens!</div>
           </Card>
-        ): (
+        ) : (
           <Card className="rounded py-10 px-12 bg-white flex-col shadow-md">
             <AnswerForm
               className="w-full mb-5"

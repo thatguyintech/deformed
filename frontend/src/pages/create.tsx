fca9f84dd0f1d4e2a83f95a4ca69db14aea9179e
ALBERT <thatguyintech@gmail.com>
2022-10-08 18:20:45 -0700

    inject web3Auth for sending transactions in create page

--- frontend/src/pages/create.tsx
+++ frontend/src/pages/create.tsx
@@ -1,7 +1,26 @@
 import { Main } from "@/templates/Main";
 import Create from "@/views/Create/Create";
+import { useWeb3Auth } from "../hooks/useWeb3Auth";
+import RPC from "../utils/web3RPC";
 
 const CreatePage = () => {
+  const {web3authProvider} = useWeb3Auth();
+
+  // TODO: call this when admin submits form creation
+  const sendTransaction = async () => {
+    if (!web3authProvider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(web3authProvider);
+    // TODO: implement the create form transaction
+    const receipt = await rpc.sendTestTransaction();
+    console.log(receipt);
+  };
+
+  // Uncomment to try out the transaction
+  // sendTransaction();
+
   return (
     <>
       <Main>

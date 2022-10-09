ffbf44f8a6a12dc451acc4560bfb5c12a2aed31b
Kei <yoshikoshikei95@gmail.com>
2022-10-09 00:25:35 -0500

    feat(frontend): populate answer form based on backend and pipe it through when creating answer

--- frontend/src/api/forms.ts
+++ frontend/src/api/forms.ts
@@ -1,9 +1,13 @@
-import { postReq } from "./ApiHelpers";
+import { getReq, postReq } from "./ApiHelpers";
 
 export const createForm = ({ fields }: any) => {
   return postReq("/forms", { data: { fields } });
 };
 
 export const createAnswer = ({ formId, formAnswers }: any) => {
-  return postReq("/forms/answers", { data: { formId, formAnswers } });
+  return postReq(`/forms/${formId}/answers`, { data: { formAnswers } });
+};
+
+export const getForm = (formId: number) => {
+  return getReq(`/forms/${formId}`);
 };

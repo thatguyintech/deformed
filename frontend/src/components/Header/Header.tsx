73e7b1e8d443661192fb3b9004ee8014922e479f
ALBERT <thatguyintech@gmail.com>
2022-10-08 18:05:07 -0700

    refactor Header component with injected web3auth

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,138 +1,14 @@
 import Button from '../Button/Button';
-import { useEffect, useState } from "react";
-import { Web3Auth } from "@web3auth/web3auth";
-import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
-import RPC from "../../utils/web3RPC";
-
-const clientId = "BKisNzJ-nHOLYH4ZrTvQhdbIjFBD0xm4AQIHoFxxm8-IlpWjVGSgiD8S8c7Soj0grFt0d4bi562ssZTdLXv8YXo"; // from https://dashboard.web3auth.io
-
+import { useWeb3Auth } from "../../hooks/useWeb3Auth";
 
 const Header = () => {
-
-  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
-  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(null);
-  const [addr, setAddr] = useState("");
-
-  useEffect(() => {
-    const init = async () => {
-      try {
-      const web3auth = new Web3Auth({
-        clientId,
-        chainConfig: {
-          chainNamespace: CHAIN_NAMESPACES.EIP155,
-          chainId: "0x1",
-          rpcTarget: "https://eth-mainnet.g.alchemy.com/v2/EGanVQmmBmhP5M7_tGsPEyI6YDQfngjZ",
-        },
-      });
-
-          setWeb3auth(web3auth);
-
-      await web3auth.initModal();if (web3auth.provider) {
-            setProvider(web3auth.provider);
-          };
-        } catch (error) {
-          console.error(error);
-        }
-      };
-
-      init();
-  }, []);
-
-  const login = async () => {
-    if (!web3auth) {
-      console.log("web3auth not initialized yet");
-      return;
-    }
-    const web3authProvider = await web3auth.connect();
-    setProvider(web3authProvider);
-    const rpc = new RPC(web3authProvider);
-    const address = await rpc.getAccounts();
-    console.log(address);
-    setAddr(address);
-  };
-
-  const getUserInfo = async () => {
-    if (!web3auth) {
-      console.log("web3auth not initialized yet");
-      return;
-    }
-    const user = await web3auth.getUserInfo();
-    console.log(user);
-  };
-
-  const logout = async () => {
-    if (!web3auth) {
-      console.log("web3auth not initialized yet");
-      return;
-    }
-    await web3auth.logout();
-    setProvider(null);
-  };
-
-  const getChainId = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const chainId = await rpc.getChainId();
-    console.log(chainId);
-  };
-  const getAccounts = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const address = await rpc.getAccounts();
-    console.log(address);
-  };
-
-  const getBalance = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const balance = await rpc.getBalance();
-    console.log(balance);
-  };
-
-  const sendTransaction = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const receipt = await rpc.sendTransaction();
-    console.log(receipt);
-  };
-
-  const signMessage = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const signedMessage = await rpc.signMessage();
-    console.log(signedMessage);
-  };
-
-  const getPrivateKey = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const privateKey = await rpc.getPrivateKey();
-    console.log(privateKey);
-  };
+  const {web3authProvider, login, logout} = useWeb3Auth();
 
   return (
     <>
       <header className="mx-40 mt-11 flex justify-between">
         <h1 className="text-3xl font-bold">Deformed</h1>
-        <Button onClick={provider ? logout : login} theme="black">{provider ? addr : "Connect Wallet"}</Button>
+        <Button onClick={web3authProvider ? logout : login} theme="black">{web3authProvider ? "Log Out": "Log In"}</Button>
       </header>
     </>
   );

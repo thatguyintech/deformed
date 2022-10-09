d8e7483d3b18f4eb5e3cff3d6c2ad2f22108a76c
Isaac Wooden <iwooden@Isaacs-MacBook-Pro.local>
2022-10-08 21:46:56 -0500

    contract restructure

--- contracts/deformed.sol
+++ contracts/deformed.sol
@@ -13,13 +13,16 @@ contract Deformed {
     }
 
     struct Form {
+        address creator;
         string configIPFSHash;
-        TokenPointer[] accessControlTokens;
-        TokenPointer[] credentials;
-        FormResponse[] responses;
     }
 
-    Form[] public forms;
+    uint256 public lastFormId;
+
+    mapping(uint256 => Form) public forms;
+    mapping(uint256 => TokenPointer[]) public accessControlTokens;
+    mapping(uint256 => TokenPointer[]) public credentials;
+    mapping(uint256 => FormResponse[]) public responses;
 
     mapping(address => uint256[]) public createdForms;
     mapping(address => uint256[]) public respondedForms;
@@ -30,35 +33,86 @@ contract Deformed {
         TokenPointer[] calldata _accessControlTokens,
         TokenPointer[] calldata _credentials
     ) public returns (uint256) {
-        Form storage newForm = forms.push();
-        for(uint i = 0; i < _accessControlTokens.length; i++) {
-            newForm.accessControlTokens.push(_accessControlTokens[i]);
-        }
-        for(uint i = 0; i < _credentials.length; i++) {
-            newForm.credentials.push(_credentials[i]);
-        }
+        Form storage newForm = forms[lastFormId];
+        newForm.creator = msg.sender;
         newForm.configIPFSHash = _configIPFSHash;
 
-        uint256 newFormId = forms.length - 1;
-        createdForms[msg.sender].push(newFormId);
-        return newFormId;
+        for(uint i = 0; i < _accessControlTokens.length; i++) {
+            accessControlTokens[lastFormId].push(_accessControlTokens[i]);
+        }
+
+        for(uint i = 0; i < _credentials.length; i++) {
+            credentials[lastFormId].push(_credentials[i]);
+        }
+
+        createdForms[msg.sender].push(lastFormId);
+
+        lastFormId++;
+        return lastFormId-1;
     }
 
     function submitFormResponse(
         uint256 formId,
         string calldata _responseHash
     ) public returns (uint256) {
-        Form storage curForm = forms[formId];
-        for(uint i = 0; i < curForm.accessControlTokens.length; i++) {
-            TokenPointer storage curTP = curForm.accessControlTokens[i];
+        for(uint i = 0; i < accessControlTokens[formId].length; i++) {
+            TokenPointer storage curTP = accessControlTokens[formId][i];
             require(IERC1155(curTP.contractAddress).balanceOf(msg.sender, curTP.tokenId) > 0,
                     "User does not own required token");
         }
-        curForm.responses.push(FormResponse(msg.sender, _responseHash));
-        uint256 newResponseId = curForm.responses.length - 1;
-
+        responses[formId].push(FormResponse(msg.sender, _responseHash));
         respondedForms[msg.sender].push(formId);
+
+        uint256 newResponseId = responses[formId].length - 1;
         responseIDs[msg.sender][formId].push(newResponseId);
         return newResponseId;
+    }
+
+    function getAccessControlTokens(uint256 formId) public view returns(TokenPointer[] memory) {
+        TokenPointer[] memory returnVal = new TokenPointer[](accessControlTokens[formId].length);
+        for(uint i = 0; i < accessControlTokens[formId].length; i++) {
+           returnVal[i] = accessControlTokens[formId][i];
+        }
+        return returnVal;
+    }
+
+    function getCredentials(uint256 formId) public view returns(TokenPointer[] memory) {
+        TokenPointer[] memory returnVal = new TokenPointer[](credentials[formId].length);
+        for(uint i = 0; i < credentials[formId].length; i++) {
+           returnVal[i] = credentials[formId][i];
+        }
+        return returnVal;
+    }
+
+    function getResponses(uint256 formId) public view returns(FormResponse[] memory) {
+        FormResponse[] memory returnVal = new FormResponse[](responses[formId].length);
+        for(uint i = 0; i < responses[formId].length; i++) {
+           returnVal[i] = responses[formId][i];
+        }
+        return returnVal;
+    }
+
+    function getCreatedForms(address _address) public view returns(uint256[] memory) {
+        uint256[] memory returnVal = new uint256[](createdForms[_address].length);
+        for(uint i = 0; i < createdForms[_address].length; i++) {
+           returnVal[i] = createdForms[_address][i];
+        }
+        return returnVal;
+    }
+
+    function getRespondedForms(address _address) public view returns(uint256[] memory) {
+        uint256[] memory returnVal = new uint256[](respondedForms[_address].length);
+        for(uint i = 0; i < respondedForms[_address].length; i++) {
+           returnVal[i] = respondedForms[_address][i];
+        }
+        return returnVal;
+    }
+
+    function getResponseIDs(address _address, uint256 formId) public view returns(uint256[] memory) {
+        uint256[] memory returnVal = new uint256[](responseIDs[_address][formId].length);
+        for(uint i = 0; i < responseIDs[_address][formId].length; i++) {
+           returnVal[i] = responseIDs[_address][formId][i];
+        }
+        return returnVal;
     }
 }
